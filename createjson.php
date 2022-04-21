bf4c1eced51c31babcb2abcd90a6228f8f3349ce
cozydevy <cozydevy@gmail.com>
2022-04-21 19:17:44 +0700

    listbox

---
+++ createjson.php
@@ -0,0 +1,19 @@
+<?php
+$post_data = array(
+  'item1' =>array(
+    'item_type_id' => 1,
+    'string_key' => 'AA',
+    'string_value' => 'Hello',
+    'string_extra' => 'App'
+
+  ),  'item2' =>array(
+    'item_type_id' => 2,
+    'string_key' => 'AA',
+    'string_value' => 'Hello',
+    'string_extra' => 'App'
+
+  )
+);
+
+echo json_encode($post_data);
+?>
